d2b22f4524c6e8fb695c2d1df22ea75512fd24de
Mazdak <nazemi.works@gmail.com>
2022-06-28 15:13:36 +0200

    more on add more routers

--- middleware/protected.go
+++ middleware/protected.go
@@ -1,15 +1,23 @@
 package middleware
 
 import (
+	"log"
+
 	"github.com/gofiber/fiber/v2"
 	jwtware "github.com/gofiber/jwt/v3"
-	app "github.com/penkong/data4life"
+	"github.com/penkong/data4life/util"
 )
 
 // Protected protect routes
 func Protected() fiber.Handler {
+
+	conf, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+
 	return jwtware.New(jwtware.Config{
-		SigningKey:   []byte(app.Conf.TokenSecret),
+		SigningKey:   []byte(conf.TokenSecret),
 		ErrorHandler: jwtError,
 	})
 }
