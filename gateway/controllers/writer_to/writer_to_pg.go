22239d0a0ff9951cf084af0aec2090494350cb2a
Mazdak <nazemi.works@gmail.com>
2022-06-30 01:14:46 +0200

    more on , writing works well going to use map to reduce that

--- gateway/controllers/writer_to/writer_to_pg.go
+++ gateway/controllers/writer_to/writer_to_pg.go
@@ -2,16 +2,11 @@ package writerto
 
 import (
 	"bufio"
-	"fmt"
+	"github.com/gofiber/fiber/v2"
+	"github.com/penkong/data4life/gateway/pkg/connect_db"
 	"log"
 	"os"
 	"sync"
-
-	// "strings"
-	// "sync"
-
-	"github.com/gofiber/fiber/v2"
-	// "github.com/penkong/data4life/gateway/pkg/connect_db"
 )
 
 func check(err error) {
@@ -22,7 +17,7 @@ func check(err error) {
 
 func WriterToPG(c *fiber.Ctx) error {
 	// init needs
-	var gNum = 8
+	var gNum = 80
 	wg := new(sync.WaitGroup)
 
 	ch := make(chan string)
@@ -31,8 +26,6 @@ func WriterToPG(c *fiber.Ctx) error {
 	src, e := os.Open("data.txt")
 	check(e)
 	defer src.Close()
-
-	wg.Add(gNum)
 
 	// reading each line of file and put it in channel
 	go func() {
@@ -44,57 +37,19 @@ func WriterToPG(c *fiber.Ctx) error {
 		}
 	}()
 
+	wg.Add(gNum)
 	for i := 0; i < gNum; i++ {
 		go func() {
 			defer wg.Done()
 			for line := range ch {
-				// connectdb.Pdb.Queries.WriteToken(c.Context(), line)
-				fmt.Println(line)
+				connectdb.Pdb.Queries.WriteToken(c.Context(), line)
 			}
 		}()
 	}
 
 	wg.Wait()
 
-	// for i := 0; i < gNum; i++ {
-	// 	wg.Add(gNum)
-	// 	go func() {
-	// 		for bfsc.Scan() {
-	// 			defer wg.Done()
-	// 			connectdb.Pdb.Queries.WriteToken(c.Context(), bfsc.Text())
-	// 		}
-	// 	}()
-	// }
-	// wg.Wait()
 	return c.Status(fiber.StatusAccepted).JSON(fiber.Map{
 		"msg": "I am writer to PG",
 	})
 }
-
-// jobs := make(chan string)
-// results := make(chan int)
-
-// wg := new(sync.WaitGroup)
-
-// go func() {
-// 	scanner := bufio.NewScanner(file)
-// 	for scanner.Scan() {
-// 		fmt.Println(scanner.Text())
-// 		jobs <- scanner.Text()
-// 	}
-// 	close(jobs)
-// }()
-
-// // Collect all the results...
-// // First, make sure we close the result channel when everything was processed
-// go func() {
-// 	wg.Wait()
-// 	close(results)
-// }()
-
-// // Now, add up the results from the results channel until closed
-// for v := range results {
-// 	fmt.Println(v)
-// }
-
-// connectdb.Pdb.Queries.WriteToken()
